c0051fabb2c2bf511d1f3c44e831985dc86a6180
boudewijnbout <boudewijn_bout@outlook.com>
2022-10-07 12:34:04 +0200

    Added hero component

---
+++ src/lib/components/IndexComponents/Hero.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let heroTitle;
+</script>
+
+<section>
+	<h2>{heroTitle}</h2>
+</section>
+
+<style>
+	section {
+		background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+			url("images/hero-image.jpeg");
+		background-size: cover;
+		background-position: center;
+		height: 15rem;
+		color: #fff;
+		padding: 1rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+</style>
