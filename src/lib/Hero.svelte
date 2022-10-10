ac71ce1b12ca5b7cfb1646d038cfafc553938f03
Emona Santiago <emonasantiago1207@gmail.com>
2022-10-10 15:54:00 +0200

    do not check my code its not done

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -4,11 +4,12 @@
 
 <section>
 	<h2>{heroTitle}</h2>
+    <p class="intro">De beoogde leerresultaten passen bij het niveau en de oriëntatie van de opleiding en zijn afgestemd op de verwachtingen van het beroepenveld en het vakgebied en op internationale eisen.</p>
 </section>
 
 <style>
 	section {
-		background-image: url("https://cmd-mid-term.netlify.app/assets/images/detail-hero-image.jpg");
+		background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("https://cmd-mid-term.netlify.app/assets/images/detail-hero-image.jpg");
 		background-size: cover;
 		background-position: center;
 		height: 15rem;
