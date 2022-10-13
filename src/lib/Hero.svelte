eb3bba9f7e323f59b33f44d36e81baa5c0a3bf72
Emona Santiago <emonasantiago1207@gmail.com>
2022-10-13 12:41:52 +0200

    hero

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -3,10 +3,8 @@
 </script>
 
 <section>
-  <h1>
-    1.0
-    {heroTitle}
-  </h1>
+  <h1>1.0</h1>
+  <h2>{heroTitle}</h2>
   <p class="intro">
     De beoogde leerresultaten passen bij het niveau en de oriëntatie van de
     opleiding en zijn afgestemd op de verwachtingen van het beroepenveld en het
@@ -21,13 +19,22 @@
 
   p {
     display: block;
-    margin-block-start: 1em;
     margin-block-end: 1em;
     margin-inline-start: 0px;
     margin-inline-end: 0px;
   }
   h1 {
     font-style: normal;
+  }
+
+  h2 {
+    display: block;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
   }
   section {
     background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
