4b488f7596d183b876e597b289737b7d2d9b87e7
boudewijnbout <boudewijn_bout@outlook.com>
2022-10-13 19:53:58 +0200

    Example v0.1

--- src/routes/[uid]/+page.svelte
+++ src/routes/[uid]/+page.svelte
@@ -1,22 +1,35 @@
 <script>
-  import ChapterSection from "$lib/components/DetailComponents/ChapterSection.svelte";
-  import ChapterContentLeft from "$lib/components/DetailComponents/ChapterContentLeft.svelte";
-  import ChapterContentRight from "$lib/components/DetailComponents/ChapterContentRight.svelte";
-  import StandardHero from "$lib/components/DetailComponents/StandardHero.svelte";
-  import StandardNavigation from "$lib/components/DetailComponents/StandardNavigation.svelte";
-  import StandardNavigationLink from "$lib/components/DetailComponents/StandardNavigationLink.svelte";
-  import Header from "../../lib/components/Header.svelte";
-  import Content from "../../lib/components/Content.svelte";
+	import ChapterSection from "$lib/components/DetailComponents/ChapterSection.svelte";
+	import ChapterContentLeft from "$lib/components/DetailComponents/ChapterContentLeft.svelte";
+	import ChapterContentRight from "$lib/components/DetailComponents/ChapterContentRight.svelte";
+	import StandardHero from "$lib/components/DetailComponents/StandardHero.svelte";
+	import StandardNavigation from "$lib/components/DetailComponents/StandardNavigation.svelte";
+	import StandardNavigationLink from "$lib/components/DetailComponents/StandardNavigationLink.svelte";
+	import Header from "../../lib/components/Header.svelte";
+	import Content from "../../lib/components/Content.svelte";
 
-  export let data;
+	export let data;
 
-  // console.log(data.standard.data.chapters[0].data.title);
+	console.log(data.standard.data.chapters);
 
-  // console.log(data.standard.data.title);
+	// console.log(data.standard.data.title);
+
+	console.log(data.standard.data.chapters);
 </script>
 
 <StandardHero
-  standardHeaderTitle={data.standard.data.title}
-  standardHeaderIntroduction={data.standard.data.introduction}
-  standardNumber={data.standard.data.standardnumber}
+	standardHeaderTitle={data.standard.data.title}
+	standardHeaderIntroduction={data.standard.data.introduction}
+	standardNumber={data.standard.data.standardnumber}
 />
+
+{#each data.standard.data.chapters as chapter, i}
+	<ChapterSection
+		chapterTitle={chapter.data.title}
+		chapterNumber={`${data.standard.data.standardnumber}.${i + 1}`}
+		chapterUID={chapter.uid}
+	>
+		<ChapterContentLeft contentLeft={chapter.data.contentleft} />
+		<ChapterContentRight contentRight={chapter.data.contentright} />
+	</ChapterSection>
+{/each}
