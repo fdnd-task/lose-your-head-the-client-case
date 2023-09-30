e64c6d07a77990296519403988b4bc19a1785350
DaanCarst <daanbrother@icloud.com>
2023-09-30 13:38:28 +0200

    view transition API

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
 	import Header from '$lib/components/header/+page.svelte';
 	import global from '../global.css'
+
+
+    import { onNavigate } from '$app/navigation'
+
+    onNavigate((navigation) => {
+    if (!document.startViewTransition) return
+
+    return new Promise((resolve) => {
+        document.startViewTransition(async () => {
+            resolve()
+            await navigation.complete
+        })
+    })
+	})
+
 </script>
+
+
 
 <main>
     <Header />
