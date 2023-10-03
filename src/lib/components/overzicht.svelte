1beec0923175118062c3fc03e9dba1f9cc500d03
Tolga1999 <tolgadoganer@outlook.com>
2023-10-03 19:00:49 +0200

    working on spacebar

--- src/lib/components/overzicht.svelte
+++ src/lib/components/overzicht.svelte
@@ -2,19 +2,28 @@
 	export let data;
 
 	import logoPartner from '$lib/assets/gvb.png';
+	import arrowRight from '$lib/assets/arrow_right.svg';
 </script>
 
 <ul>
 	{#each data.websites as website}
 		<li>
-			<section>
-				<img src={logoPartner} alt="logo partner" />
-				<h2>{website.titel}</h2>
+			<section class="logo-partner-section">
+				<div>
+					<img src={logoPartner} alt="logo partner" />
+					<h2>{website.titel}</h2>
+				</div>
+				<img src={arrowRight} alt="arrow right" />
 			</section>
 
 			<section class="more-info-section">
 				<span>Website: {website.homepage}</span>
-				<span>Laatst bewerkt: 12min. geleden</span>
+				<span>Laatst bewerkt: 12 min. geleden</span>
+
+				<div class="progress-container">
+					<progress id="progress-partner" max="100" value="70" />
+					<label for="progress-partner">70%</label>
+				</div>
 			</section>
 		</li>
 	{/each}
@@ -29,10 +38,10 @@
 	}
 
 	li {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        gap: 2.25em;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		gap: 2.25em;
 
 		background-color: var(--c-container);
 		padding: 1em;
@@ -41,14 +50,31 @@
 		min-width: 20em;
 	}
 
-	h2 {
-		font-size: 1.5em;
-        margin-top: 0.05em;
+	.logo-partner-section {
+		display: flex;
+		align-items: flex-start;
+		justify-content: space-between;
 	}
 
-    .more-info-section{
-        display: flex;
-        flex-direction: column;
-        font-size: 0.9em;
-    }
+	h2 {
+		font-size: 1.5em;
+		margin-top: 0.05em;
+	}
+
+	.more-info-section {
+		display: flex;
+		flex-direction: column;
+		font-size: 0.9em;
+	}
+
+	.progress-container{
+		display: flex;
+		justify-content: space-between;
+		gap: 1em;
+		margin-top: 0.25em;
+	}
+
+	progress{
+		width: 100%;
+	}
 </style>
