39f6bfe10bd8e4909a61329846f5e6d0c858121e
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-10-09 09:22:31 +0200

    footer update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,12 +24,26 @@
 
 <style>
 
+    /* :root {
+        --charcoal: #051A1A;
+        --candelLight: #FFDA1D;
+        --justWhite: #FFFFFF;
+        --jungleGreen: #163A3A;
+        --plantGreen: #179446;
+        --pictonBlue: #3DBAE1;
+        --copy-font: 'Apercu pro', sans-serif;
+        --paragraph-font: 'Jubilat', serif;
+        --header-font: 'Kirang Haerang', sans-serif;
+        --link-hover-color: #FFDA1D;
+        --button-hover-color: #FFFFFF;
+        --top-nav-sponsor-button-hover-color: #051A1A;
+    } */
 
-h1 {
-    font-size: 5em;
-    color: var(--justWhite);
-    text-transform: uppercase;
-    margin: 2em 0;
-}
+    h1 {
+        font-size: 5em;
+        color: var(--justWhite);
+        text-transform: uppercase;
+        margin: 2em 0;
+    }
 
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -30,6 +30,23 @@
 
 <style>
 
+
+    /* :root {
+        --charcoal: #051A1A;
+        --candelLight: #FFDA1D;
+        --justWhite: #FFFFFF;
+        --jungleGreen: #163A3A;
+        --plantGreen: #179446;
+        --pictonBlue: #3DBAE1;
+        --copy-font: 'Apercu pro', sans-serif;
+        --paragraph-font: 'Jubilat', serif;
+        --header-font: 'Kirang Haerang', sans-serif;
+        --link-hover-color: #FFDA1D;
+        --button-hover-color: #FFFFFF;
+        --top-nav-sponsor-button-hover-color: #051A1A;
+    } */
+
+
     h1, h2{
         font-size: 5em;
         display: inline-block;
