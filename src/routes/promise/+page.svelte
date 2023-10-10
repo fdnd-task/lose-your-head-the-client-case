469795609a3ed4780da2b4a97073b9991d203787
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-10-10 21:31:36 +0200

    images

--- src/routes/promise/+page.svelte
+++ src/routes/promise/+page.svelte
@@ -12,24 +12,29 @@
 <main>
 
     {#each data.promisePages as promisePage }
-
+   
+    
         <h1>{promisePage.promiseIntro.title}</h1>
+        <img src="/static/Subtract.svg" alt="svg image">
 
+<div class="block">
         <p class="text1"> {promisePage.promiseIntro.contentText}</p>
-
         <p class="text2"> {promisePage.promiseIntro.contentText2}</p>
-       
+</div>
+    
         <img src="{promisePage.promiseIntro.contentImg.url}" alt="content" class="responsive">
+       
     {/each}
 
+  
 
     {#each data.promisePages as promisePage }
 
-        <h1>{promisePage.promiseMain.title}</h1>
-
+    <img src="/static/Subtract.svg" alt="svg image">
+        <h2>{promisePage.promiseMain.title}</h2>
         <p class="text3"> {promisePage.promiseMain.contentText}</p>
-
-        <img src="{promisePage.promiseMain.imgContent.url}" alt="content" class="responsive">
+ 
+        <!-- <img src="{promisePage.promiseMain.imgContent.url}" alt="content" class="responsive"> -->
 
     {/each}
 
@@ -42,7 +47,11 @@
 
 <style>
 
-    /* :root {
+*{
+    box-sizing: border-box;
+    line-height: 1.4;
+}
+    :root {
         --charcoal: #051A1A;
         --candelLight: #FFDA1D;
         --justWhite: #FFFFFF;
@@ -55,11 +64,11 @@
         --link-hover-color: #FFDA1D;
         --button-hover-color: #FFFFFF;
         --top-nav-sponsor-button-hover-color: #051A1A;
-    } */
+    }
 
 
     /*mobile first*/
-    h1{
+    h1, h2{
         font-size: 5em;
         width:30em;
         padding: 1rem;
@@ -67,22 +76,64 @@
 
    .text1, .text2, .text3{
         padding: 1em;
-        width: 30em;
         font-size: 2em;
-    }
-
-  .responsive{
-    padding:4rem;
-   
+        width: 30em;
        
+    }
+    .block{
+        background-color: var(--jungleGreen);
+        clip-path: polygon(16% 0, 100% 13%, 100% 41%, 100% 83%, 0 98%, 0 25%);
+        margin-top: 7em;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+       
+       
+       
+    }
+     
+
+     .responsive{
+    clip-path: polygon(19% 9%, 100% 0, 100% 41%, 100% 83%, 0 98%, 0 11%);
+   
+  
+  
+}
+    .shapes{
+     
+        background-image:url("/static/Subtract.svg");
+        z-index: 1;
+
+        
+    }
+  
+
+   @media only screen and (min-width: 1200px) {
+    .responsive {
+   width:100%;
+   height:auto;
+   clip-path: polygon(19% 9%, 100% 0, 100% 41%, 100% 83%, 0 98%, 0 11%);
+}
+   
+
+
+  .text1, .text2, .text3{
+    margin: 0 auto;
+    width:30em;
+    font-size: 1.5em;
+  }
+
+
+  h1{
+    margin: 0 auto;
+  }
+   
+  
+   .block{
+    padding-bottom: 50px;
+        padding-top:30px;
+       
+
    }
 
-   @media only screen (max-width: 600px) {
-  .responsive {
-   display:none;
-   width:100%;
-  }
-}
-
+   }
 
 </style>
