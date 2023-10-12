7b5baa849a4004c6d8197f98541f7c24739a65ed
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-10-12 12:21:09 +0200

    .

--- src/routes/promise/+page.svelte
+++ src/routes/promise/+page.svelte
@@ -12,27 +12,30 @@
 <main>
 
     {#each data.promisePages as promisePage }
-   
     
         <h1>{promisePage.promiseIntro.title}</h1>
+
+        <div class="containter-grid">
+               <div class="grid-block"></div>
+        </div>
+
         
-        <div class="block-text-2">
-<section class="block-text" >
-<!-- <div class="block"> -->
-        <p class="text1"> {promisePage.promiseIntro.contentText}</p>
-        <p class="text2"> {promisePage.promiseIntro.contentText2}</p>
-<!-- </div> -->
-</section>
-</div>
- <img src="{promisePage.promiseIntro.contentImg.url}" alt="content" class="img">
-       
+        <section class="wrapper">
+            <div class="block-text" > 
+                <p class="text1"> {promisePage.promiseIntro.contentText}</p>
+                <p class="text2"> {promisePage.promiseIntro.contentText2}</p>
+            </div>
+        </section>
+
+        <img src="{promisePage.promiseIntro.contentImg.url}" alt="content" class="img">
+
     {/each}
 
   
 
     {#each data.promisePages as promisePage }
 
-   <div class="block-text-2">
+   <div class="grid-block">
         <h2>{promisePage.promiseMain.title}</h2>
         <p class="text3"> {promisePage.promiseMain.contentText}</p>
     </div>
@@ -99,7 +102,7 @@
     }
     section{
     display:grid;
-	grid-template-rows:auto 8em auto;
+	grid-template-rows:auto 10em auto;
     grid-row-start:1;
 	grid-row-end:3;
 	grid-column-start:1;
@@ -137,53 +140,74 @@
 
 }
 
-.block-text-2 {
-    background-image: url("/Subtract.png");
+.containter-grid{
     width:100%;
-    height:-50%;
-    /* opacity: .5; */
-    object-fit: contain;
-   
+    height: 50em;
+}
 
+.grid-block {
+    background-image: url(/Subtract.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 56%;
+    height: 29em;
+  
 }
 
 
-   @media only screen and (min-width: 768px) and (max-width: 1200px) {
-    .responsive {
+   @media only screen and (min-width: 1200px) {
+    .img {
    width:100%;
-   height:auto;
+   height:50em;
    clip-path: polygon(19% 9%, 100% 0, 100% 41%, 100% 83%, 0 98%, 0 11%);
+  
+
 }
    
-
-
   .text1, .text2, .text3{
     margin: 0 auto;
-    font-size: 1em;
+    font-size: 1.2em;
+   
   }
+  
 
 
   h1{
     margin: 0 auto;
-    align-items:center;
+    font-size: 4em;
+    
   }
    h2{
     margin: 0 auto;
-    align-items:center;
+  
+   
    }
   
    .block{
     padding-bottom: 50px;
         padding-top:30px;
-       
-
+        width:100%;
+        /* clip-path: polygon(19% 9%, 100% 0, 100% 41%, 100% 83%, 0 98%, 0 15%); */
+        clip-path: 
+        polygon(
+			0% 4em,
+			30% 0,
+			100% 4.5em,
+			100% calc(100% - 2em),
+			0% 100%,
+			0% 4em
+		);
+   
+   }
+   .grid-block{
+       margin: 0 auto;
    }
 
-   .block-text-2{
-    padding-bottom: 50px;
+   /* .block-text-2{
+    padding-bottom: 30px;
         padding-top:30px;
 
-   }
+   } */
    }
 
 </style>
