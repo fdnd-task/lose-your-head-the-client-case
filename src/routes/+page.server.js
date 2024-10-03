729a4d0afd2cb99668f927bd40f81c3b0ae3eaed
SamaraFellaDina <sammybensink@gmail.com>
2024-10-03 18:07:40 +0200

    #58 Verbeteren van de code
    Er miste een aantal puntcomma's, dus die heb ik hier verbeterd

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,13 @@
-import fetchJson from "$lib/fetch-json"
+import fetchJson from "$lib/fetch-json";
 
 export async function load() {
 
-  const API = 'https://fdnd-agency.directus.app/items/'
+  const API = 'https://fdnd-agency.directus.app/items/';
   const urlSites = API + 'frd_site';
   const urlScans = API + 'frd_scans';
 
-  const sites = await fetchJson(urlSites)
-  const scans = await fetchJson(urlScans)
+  const sites = await fetchJson(urlSites);
+  const scans = await fetchJson(urlScans);
   
   return {
     sites : sites.data,
