7802cf502811e810176a3f25e0ec79a0cb12de9e
SamaraFellaDina <sammybensink@gmail.com>
2024-10-03 18:15:45 +0200

    #57 verbeteren van api variabelen
    Na commit 332b5da, liet @joostf zien dat het net ietsje anders kon. Super logische code! Was even bij mij voorbij gegaan. Dus bij deze! Ook vertelt @joostf iets over `semicolon`. Ik weet niet precies wat dat is dus daar kijk ik op een ander moment naar. :)

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,8 +3,8 @@ import fetchJson from "$lib/fetch-json";
 export async function load() {
 
   const API = 'https://fdnd-agency.directus.app/items/';
-  const urlSites = API + 'frd_site';
-  const urlScans = API + 'frd_scans';
+  const urlSites = `${API}frd_site`
+  const urlScans = `${API}frd_scans`;
 
   const sites = await fetchJson(urlSites);
   const scans = await fetchJson(urlScans);
