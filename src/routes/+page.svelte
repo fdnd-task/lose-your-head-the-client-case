85fbe46475f9dc7c628ac26edf43d506d59f12a1
TomDeeterink1 <deeterinktom@gmail.com>
2024-10-04 15:05:31 +0200

    commit with images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,19 +14,4 @@
     <h1>lose-your-head-the-client-case</h1>
     
     </main>
-<script>
-// variables imports
 
-// component imports
-
-</script>
-
-<style>
-
-</style>
-
-<main>
-
-<h1>lose-your-head-the-client-case</h1>
-
-</main>
