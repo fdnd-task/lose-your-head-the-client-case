209beb9154b2c016cb354b39885d3e8d0d8a3eb2
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-10-04 14:26:08 +0200

    JC - Fixed height footer
    Heb de footer aangepast zodat het onderste gedeelte niet buiten de footer komt

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -7,39 +7,42 @@
 </script>
 
 <footer class="footer">
-    <enhanced:img src={Veronica} class="footer__veronica" alt="logo van veronica in footer" />
-    <h2 class="footer__h2">We. Love. Music</h2>
-    <ul class="footer__socials">
-        <h3 class="footer__h3">volg ons op social media</h3>
-        <ul class="footer__socials-icons">
-            <li class="footer__icons"><enhanced:img src={Facebook}  alt="socials icoon Facebook" /></li>
-            <li class="footer__icons"><enhanced:img src={X} alt="socials icoon X" /></li>
-            <li class="footer__icons"><enhanced:img src={YT} alt="social icoon Youtube" /></li>
-            <li class="footer__icons"><enhanced:img src={IG} alt="social icoon Instagram" /></li>
+    <div class="footer__grid">
+        <enhanced:img src={Veronica} class="footer__veronica" alt="logo van veronica in footer" />
+        <h2 class="footer__h2">We. Love. Music</h2>
+        <ul class="footer__socials">
+            <h3 class="footer__h3">volg ons op social media</h3>
+            <ul class="footer__socials-icons">
+                <li class="footer__icons"><enhanced:img src={Facebook}  alt="socials icoon Facebook" /></li>
+                <li class="footer__icons"><enhanced:img src={X} alt="socials icoon X" /></li>
+                <li class="footer__icons"><enhanced:img src={YT} alt="social icoon Youtube" /></li>
+                <li class="footer__icons"><enhanced:img src={IG} alt="social icoon Instagram" /></li>
+            </ul>
         </ul>
-    </ul>
-    <ul class="footer__radio-veronica">
-        <h3 class="footer__h3">Radio Veronica</h3>
-        <li class="footer__radio-links">Home</li>
-        <li class="footer__radio-links">Programmering</li>
-        <li class="footer__radio-links">Ontdek</li>
-        <li class="footer__radio-links">Acties</li>
-    </ul>
-    <ul class="footer__algemeen">
-        <h3 class="footer__h3">Algemeen</h3>
-        <li class="footer__algemeen-links">Frequenties</li>
-        <li class="footer__algemeen-links">Alle programma's</li>
-        <li class="footer__algemeen-links">DJ overzicht</li>
-        <li class="footer__algemeen-links">Pers</li>
-    </ul>
-    <span class="footer__underline"></span>
-    <p class="footer__copyright">&copy; Mediahuis 2024</p>
-    <ul class="footer__FAQ">
-        <li class="footer__FAQ-links">Privacy verklaring</li>
-        <li class="footer__FAQ-links">Gebruiksvoorwaarden</li>
-        <li class="footer__FAQ-links">Cookieverklaring</li>
-        <li class="footer__FAQ-links">Cookie voorkeuren</li>
-    </ul>
+        <ul class="footer__radio-veronica">
+            <h3 class="footer__h3">Radio Veronica</h3>
+            <li class="footer__radio-links">Home</li>
+            <li class="footer__radio-links">Programmering</li>
+            <li class="footer__radio-links">Ontdek</li>
+            <li class="footer__radio-links">Acties</li>
+        </ul>
+        <ul class="footer__algemeen">
+            <h3 class="footer__h3">Algemeen</h3>
+            <li class="footer__algemeen-links">Frequenties</li>
+            <li class="footer__algemeen-links">Alle programma's</li>
+            <li class="footer__algemeen-links">DJ overzicht</li>
+            <li class="footer__algemeen-links">Pers</li>
+        </ul>
+        <span class="footer__underline"></span>
+        <p class="footer__copyright">&copy; Mediahuis 2024</p>
+        <ul class="footer__FAQ">
+            <li class="footer__FAQ-links">Privacy verklaring</li>
+            <li class="footer__FAQ-links">Gebruiksvoorwaarden</li>
+            <li class="footer__FAQ-links">Cookieverklaring</li>
+            <li class="footer__FAQ-links">Cookie voorkeuren</li>
+        </ul>
+    </div>
+
 </footer>
 
 <style>
@@ -48,16 +51,20 @@
     }
 
     .footer {
+        background-color: var(--color-footer);
+        padding: 3.5rem;
+    }
+
+    .footer__grid {
         font-family: var(--font-family);
         color: var(--light);
-        background-color: var(--color-footer); 
+
         display: grid;
-        height: 50vh;
         grid-template-columns: repeat(4, 0.5fr);
         grid-template-rows: repeat(5, 1fr);
-        padding: 3.5rem;
+
         gap: 2rem;
-        grid-template-areas: 
+        grid-template-areas:
         "veronica . . ."
         "header-footer . radio-veronica algemeen"
         "socials . radio-veronica algemeen"
@@ -98,7 +105,7 @@
         height: 0.1rem;
         background-color: var(--secondary-color);
     }
-    
+
     .footer__socials {
         grid-area: socials;
         display: flex;
