30f4c40507c1f93bc8b4e3b1faa0b414acbc39eb
Daan Jacobs <daan@videotechniek.nl>
2024-11-19 15:17:00 +0100

    deleted unecessary pages folder in lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
     import Header from "../lib/organism/Header.svelte";
     import Tabs from '../lib/molecules/tabs.svelte';
-  import Mobilenav from "../lib/organism/Mobilenav.svelte";
+    import Mobilenav from "../lib/organism/Mobilenav.svelte";
     import Footer from '../lib/organism/Footer.svelte';
 </script>
-
 
 <Header/>
 <Tabs />
