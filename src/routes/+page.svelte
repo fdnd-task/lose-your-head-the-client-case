c7d8cf7c0e57aca6578362c01d4ae7f54a5e3f2a
TomDeeterink1 <deeterinktom@gmail.com>
2024-11-20 20:25:55 +0100

    Push van tab button #71
    In deze push heb ik het component gemaakt voor de bottom menu. Dit is een atoom kwa grote.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
     import Schedule from "../lib/molecules/schedule.svelte";
     import WeekSelectie from '../lib/molecules/WeekSelectie.svelte';
-  
+    import Tabmenu_link from '../lib/atoms/Tabmenu_link.svelte';
     export let data;
 </script>
 
 <main>
     <WeekSelectie/>
-    <Schedule {data}/>
+    <Schedule {data}/> 
 </main>
