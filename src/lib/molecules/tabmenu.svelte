e4683e08dfebd798722cff9e23e2b1edffd4a751
TomDeeterink1 <deeterinktom@gmail.com>
2024-11-20 21:24:26 +0100

    commit #71
    In deze commit zorg ik ervoor dat de tabmenu_link, correct wordt ingeladen in het tabmenu.

---
+++ src/lib/molecules/tabmenu.svelte
@@ -0,0 +1,21 @@
+<script>
+import Tabmenu_link from '../atoms/Tabmenu_link.svelte';
+</script>
+
+<ul>
+    <Tabmenu_link label="home" icon="home"/>
+    <Tabmenu_link label="ontdek" icon="ontdek"/>
+    <Tabmenu_link label="radio" icon="radio"/>
+    <Tabmenu_link label="meer" icon="meer"/>
+</ul>
+
+<style>
+    ul{
+        border-top: 1px solid #EDEDED;
+        background-color: var(--light);
+        width: 100%;
+        padding: 1em 0em;
+        display: flex;
+        width: 100%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
+    import Tabmenu from '../lib/molecules/Tabmenu.svelte';
     import Schedule from "../lib/molecules/schedule.svelte";
     import WeekSelectie from '../lib/molecules/WeekSelectie.svelte';
-    import Tabmenu_link from '../lib/atoms/Tabmenu_link.svelte';
+ 
     export let data;
 </script>
 
 <main>
     <WeekSelectie/>
     <Schedule {data}/> 
+    <Tabmenu/>
 </main>
